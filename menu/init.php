8fc6f54a9f5ca71c5a6675c08543093e6c213849
randyjensen <randyjensen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-14 16:40:31 +0000

    updated css for left nav icon for wordpress 2.8
    git-svn-id: https://plugins.svn.wordpress.org/rj-quickcharts/trunk@821949 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/init.php
+++ menu/init.php
@@ -46,16 +46,30 @@ if(!class_exists('RJ_Quickcharts_Menu'))
 
         public function css_for_rjqc_admin_menu() {
             echo '
-            <style type="text/css" media="screen">
-            #toplevel_page_rj-quickcharts-admin-rjqc-admin div.wp-menu-image {
-                background: url('.plugins_url().'/rj-quickcharts/img/nav-chart.png) no-repeat 6px -17px !important;
+            <style type="text/css" media="screen">';
+            if (get_bloginfo('version') >= 2.8) {
+                echo
+                '#toplevel_page_rj-quickcharts-admin-rjqc-admin div.wp-menu-image {
+                    background: url('.plugins_url().'/rj-quickcharts/img/nav-chart.png) no-repeat 10px -15px !important;
+                }
+                #toplevel_page_rj-quickcharts-admin-rjqc-admin:hover div.wp-menu-image,
+                #toplevel_page_rj-quickcharts-admin-rjqc-admin.wp-has-current-submenu div.wp-menu-image {
+                    background-position: 10px 9px!important;
+                }
+                #adminmenu  #toplevel_page_rj-quickcharts-admin-rjqc-admin div.wp-menu-image:before {
+                    content: "";
+                }';
+            } else {
+                echo
+                '#toplevel_page_rj-quickcharts-admin-rjqc-admin div.wp-menu-image {
+                    background: url('.plugins_url().'/rj-quickcharts/img/nav-chart.png) no-repeat 6px -17px !important;
+                }
+                #toplevel_page_rj-quickcharts-admin-rjqc-admin:hover div.wp-menu-image,
+                #toplevel_page_rj-quickcharts-admin-rjqc-admin.wp-has-current-submenu div.wp-menu-image {
+                    background-position:6px 7px!important;
+                }';
             }
-            #toplevel_page_rj-quickcharts-admin-rjqc-admin:hover div.wp-menu-image,
-            #toplevel_page_rj-quickcharts-admin-rjqc-admin.wp-has-current-submenu div.wp-menu-image {
-                background-position:6px 7px!important;
-            }
-            </style>
-            ';
+            echo '</style>';
         }
     }
 }
